e6b28b38c7145de11a5e4102cc13797086fdce62
sakusaku3939 <yuukiyuuki223@gmail.com>
2026-06-18 01:33:57 +0900

    Trigger golden report comment

--- lib/src/core/design/app_tokens.dart
+++ lib/src/core/design/app_tokens.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppTokens {
   const AppTokens._();
 
-  static const Color primary = Color(0xFF000000);
+  static const Color primary = Color(0xFF2563EB);
   static const Color background = Color(0xFFF8FAFC);
   static const Color surface = Color(0xFFFFFFFF);
   static const Color text = Color(0xFF111827);
