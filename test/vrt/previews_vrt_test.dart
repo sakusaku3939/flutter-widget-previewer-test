6634e150c2542141e4c0f2eee366a13832470434
sakusaku3939 <yuukiyuuki223@gmail.com>
2026-06-18 23:10:24 +0900

    Load app fonts in Flutter golden tests

--- test/vrt/previews_vrt_test.dart
+++ test/vrt/previews_vrt_test.dart
@@ -1,9 +1,16 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:widget_previewer_lab/src/presentation/previews/foundation/preview_surface.dart';
 import 'package:widget_previewer_lab/src/presentation/previews/vrt_previews.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(_loadAppFonts);
+
   group('preview VRT', () {
     for (final preview in visualRegressionPreviews) {
       testWidgets('${preview.group} ${preview.name}', (tester) async {
@@ -36,4 +43,22 @@ void main() {
       }, tags: 'golden');
     }
   });
+}
+
+Future<void> _loadAppFonts() async {
+  final fontManifestJson = await rootBundle.loadString('FontManifest.json');
+  final fontManifest = json.decode(fontManifestJson) as List<dynamic>;
+
+  for (final fontFamilyManifest in fontManifest) {
+    final fontFamily = fontFamilyManifest as Map<String, dynamic>;
+    final loader = FontLoader(fontFamily['family'] as String);
+    final fonts = fontFamily['fonts'] as List<dynamic>;
+
+    for (final fontManifest in fonts) {
+      final font = fontManifest as Map<String, dynamic>;
+      loader.addFont(rootBundle.load(font['asset'] as String));
+    }
+
+    await loader.load();
+  }
 }
